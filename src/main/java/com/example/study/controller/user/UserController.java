1a429c1406edf2fe967a0d7b0fc251db257889fb
haru <haru.hwany@gmail.com>
2025-10-28 21:20:34 +0900

    Fix: register reponse 수정

--- src/main/java/com/example/study/controller/user/UserController.java
+++ src/main/java/com/example/study/controller/user/UserController.java
@@ -1,9 +1,11 @@
 package com.example.study.controller.user;
 
 import com.example.study.controller.user.request.auth.UserRegisterRequest;
+import com.example.study.controller.user.response.ApiResponse;
 import com.example.study.controller.user.response.auth.UserRegisterResponse;
 import com.example.study.domain.UserRepository;
 import com.example.study.service.user.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,21 +25,35 @@ public class UserController {
     public ResponseEntity<?> registerUser(@RequestBody UserRegisterRequest request) {
         UserRegisterResponse userRegisterResponse = userService.registerUser(request.toServiceDto());
 
-        /* todo http response DTO 만들기  */
-        /* todo 각 응답 코드 기준? 정하기 */
         /* todo 브랜치 생성 및 관리 */
         if (!userRegisterResponse.isSuccess()) {
             if (userRegisterResponse.getMessage().equals("username")) {
-                return ResponseEntity.status(409).body("아이디 중복");
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                        ApiResponse.builder()
+                                .status(HttpStatus.CONFLICT.value())
+                                .message("이미 사용중인 아이디 입니다.")
+                                .build());
 
             } else if (userRegisterResponse.getMessage().equals("nickname")) {
-                return ResponseEntity.status(409).body("닉네임 중복");
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                        ApiResponse.builder()
+                        .status(HttpStatus.CONFLICT.value())
+                        .message("이미 사용중인 닉네임 입니다.")
+                        .build());
 
             } else if(userRegisterResponse.getMessage().equals("email")) {
-                return ResponseEntity.status(409).body("이메일 중복");
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                        ApiResponse.builder()
+                                .status(HttpStatus.CONFLICT.value())
+                                .message("이미 사용중인 이메일 입니다.")
+                                .build());
             }
         }
 
-        return ResponseEntity.ok(userRegisterResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.builder()
+                        .status(HttpStatus.CREATED.value())
+                        .data(userRegisterResponse)
+                        .build());
     }
 }
