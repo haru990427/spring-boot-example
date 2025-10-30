8d93235d2c52a9462805ffd472ec94c4388da4ff
haru <haru.hwany@gmail.com>
2025-10-30 17:26:22 +0900

    Feat: 회원가입 로깅 및 예외 추가(race condition 고려)

--- src/main/java/com/example/study/service/user/UserService.java
+++ src/main/java/com/example/study/service/user/UserService.java
@@ -7,46 +7,62 @@ import com.example.study.domain.UserRepository;
 import com.example.study.domain.enums.EmailStatus;
 import com.example.study.domain.enums.Role;
 import com.example.study.service.user.dto.UserRegisterDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+    /* return 중복 필드 */
+    private String extractField(DataIntegrityViolationException e) {
+        String message = e.getMostSpecificCause().getMessage().toLowerCase();
+
+        if (message.contains("username")) return "username";
+        if (message.contains("nickname")) return "nickname";
+        if (message.contains("email")) return "email";
+
+        return "duplicate: " + message;
     }
 
-
-
+    @Transactional
     public UserRegisterResponse createUser(UserRegisterDTO userData) {
-        if (this.userRepository.findByUsername(userData.getUsername()).isPresent()) {
+        log.info("회원가입 시작 - 아이디: {}", userData.getUsername());
+
+        if (this.userRepository.existsByUsername(userData.getUsername())) {
              /* 아이디 중복 */
+            log.warn("아이디 중복 탐지");
+            log.warn("이미 가입된 아이디: {}",  userData.getUsername());
             return UserRegisterResponse.builder()
                     .success(false)
                     .userInfo(null)
                     .message("username")
                     .build();
-
         }
 
-        if (this.userRepository.findByNickname(userData.getNickname()).isPresent()) {
+        if (this.userRepository.existsByNickname(userData.getNickname())) {
             /* 닉네임 중복 */
+            log.warn("닉네임 중복 탐지");
+            log.warn("이미 가입된 닉네임: {}", userData.getNickname());
             return UserRegisterResponse.builder()
                     .success(false)
                     .userInfo(null)
                     .message("nickname")
                     .build();
-
         }
 
-        if (this.userRepository.findByEmail(userData.getEmail()).isPresent()) {
+        if (this.userRepository.existsByEmail(userData.getEmail())) {
             /* 이메일 중복 */
+            log.warn("이메일 중복 탐지");
+            log.warn("이미 가입된 이메일: {}",  userData.getEmail());
             return UserRegisterResponse.builder()
                     .success(false)
                     .userInfo(null)
@@ -55,6 +71,8 @@ public class UserService {
         }
 
         String encodedPassword = passwordEncoder.encode(userData.getPassword());
+        log.info("비밀번호 인코딩 완료");
+
         User newUser = User.builder()
                 .username(userData.getUsername())
                 .password(encodedPassword)
@@ -63,28 +81,46 @@ public class UserService {
                 .email(userData.getEmail())
                 .emailStatus(EmailStatus.UNVERIFIED)
                 .build();
+        log.info("사용자({}) 객체 생성 완료", userData.getUsername());
+
         try {
-            this.userRepository.save(newUser);
+            User savedUser = this.userRepository.save(newUser);
+            log.info("사용자({}) DB 저장 완료",  userData.getUsername());
+
+            UserInfoResponse userInfoResponse = UserInfoResponse.builder()
+                    .userID(savedUser.getId())
+                    .userName(savedUser.getUsername())
+                    .nickName(savedUser.getNickname())
+                    .role(savedUser.getRole())
+                    .email(savedUser.getEmail())
+                    .emailStatus(savedUser.getEmailStatus())
+                    .createdAt(savedUser.getCreatedAt())
+                    .updatedAt(savedUser.getUpdatedAt())
+                    .build();
+
+            return UserRegisterResponse.builder()
+                    .success(true)
+                    .userInfo(userInfoResponse)
+                    .message(null)
+                    .build();
+        } catch (DataIntegrityViolationException e) { // 코드로 중복 검사 해도 예외 처리 (race condition)
+            log.warn("경합 조건 발생 -> 이미 가입된 계정 필드 감지: {}", e.getMessage());
+            String field = extractField(e);
+            log.warn("중복된 필드: {}", field);
+
+            return UserRegisterResponse.builder()
+                    .success(false)
+                    .userInfo(null)
+                    .message(field)
+                    .build();
         } catch (Exception e) {
-            /* 오류 발생 */
+            log.error("회원가입 중 예기치 못한 오류 발생 - 오류가 발생한 아이디: {}", userData.getUsername(), e);
+
+            return UserRegisterResponse.builder()
+                    .success(false)
+                    .userInfo(null)
+                    .message("something went wrong")
+                    .build();
         }
-
-        UserInfoResponse userInfoResponse = UserInfoResponse.builder()
-                .userID(newUser.getId())
-                .userName(newUser.getUsername())
-                .nickName(newUser.getNickname())
-                .role(newUser.getRole())
-                .email(newUser.getEmail())
-                .emailStatus(newUser.getEmailStatus())
-                .createdAt(newUser.getCreatedAt())
-                .updatedAt(newUser.getUpdatedAt())
-                .build();
-
-        return UserRegisterResponse.builder()
-                .success(true)
-                .userInfo(userInfoResponse)
-                .message(null)
-                .build();
     }
-
 }
